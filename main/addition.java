f49560d853f295e06f4bb78d084bdf2eb6ec9c65
Shivu <sshivukumar7@gmail.com>
2023-03-30 16:46:20 +0530

    added some code

--- main/addition.java
+++ main/addition.java
@@ -1,4 +1,22 @@
 package main;
 
 public class addition {
+
+    public void test(){
+        int a = 1, b=2;
+        int sum = a+b;
+
+    }
+
+    public void test1(){
+        int a = 1, b=2;
+        int sum = a+b;
+
+    }
+
+    public void test2(){
+        int a = 1, b=2;
+        int sum = a+b;
+
+    }
 }
